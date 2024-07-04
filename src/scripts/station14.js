d0a1031bac8dc526cb45f575facab3799b0e7bf4
ここにユーザー名を入力 <>
2024-07-04 17:19:16 +0900

    a

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,7 +6,7 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+    return test.map(buildFullName);
 }
 
 function buildFullName(data) {
@@ -23,4 +23,4 @@ function buildFullName(data) {
 //     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affiliation: 'HogeHoge大学', is_student: true}
 // ];
 
-document.write(getData())
+document.write(buildFullName())
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,25 @@
 async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
   const userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+
+  const full_name = userList.map((user) => {
+    return user.family_name + ' ' + user.first_name;
+  });
+
+  const result = await test(full_name);
+  return result;
 }
 
-function test() {
-  return
+function test(full_name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(full_name);
+    }, 3000);
+  });
 }
 
+getData().then((result) => {
+  console.log(result);
+});
